78f2b5b3bb5035968ac35bcf133fb857bd18c3d0
firstdreamding <huaxu3721@yahoo.com>
2018-08-10 23:42:05 -0500

    Section

---
+++ LudumOutOfSpace/src/game/level/Section.java
@@ -0,0 +1,17 @@
+package game.level;
+
+import game.graphics.Screen;
+
+public class Section {
+	
+	protected int height;
+	protected int id;
+	
+	public void render(Screen screen) {
+		
+	}
+	
+	public void update() {
+		
+	}
+}
---
+++ LudumOutOfSpace/src/game/level/Section1.java
@@ -0,0 +1,15 @@
+package game.level;
+
+import game.graphics.Screen;
+
+public class Section1 extends Section {
+	
+	
+	public void render(Screen screen) {
+
+	}
+
+	public void update() {
+
+	}
+}
