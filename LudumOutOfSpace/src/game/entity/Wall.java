2c4f82b47f0567d36c5ca2e4aa4543f906b9888c
gemeLn <mtang317@gmail.com>
2018-08-10 21:19:42 -0500

    919

--- LudumOutOfSpace/src/game/entity/Wall.java
+++ LudumOutOfSpace/src/game/entity/Wall.java
@@ -4,17 +4,24 @@ import game.Game;
 import game.graphics.Screen;
 
 public class Wall extends Entity {
-	boolean left;
+	boolean right;
+	int speed = 1;
 
-	public Wall(boolean left) {
-		this.left = left;
+	public Wall(boolean right) {
+		this.right = right;
+		if (right)
+			x = Game.getWindowWidth()/2;
+		h = Game.getWindowHeight();
 	}
 
 	public void update() {
+		w += speed;
+		if (right)
+			x -= speed;
+
 	}
 
 	public void render(Screen screen) {
-		screen.drawRect(0, 0, 0, Game.getWindowHeight(), 0xffffff, true);
-		
+		screen.drawRect(x, 0, w, h, 0xffffff, false);
 	}
 }
